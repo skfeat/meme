f54d2258a6e5af76aa5b1860cbbe94dc61097fdc
skfeat <128950274+skfeat@users.noreply.github.com>
2023-03-26 23:04:11 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
-
-
-
 const fetchedUrls = [];
 
 function fetchMeme() {
   fetch("https://meme-api.com/gimme/animememes/1")
     .then(response => response.json())
-    .then(meme => {
-      const url = meme["memes*]["url"];
+    .then(data => {
+      const meme = data.memes[0];
+      const url = meme.url;
       if (fetchedUrls.includes(url)) {
         fetchMeme(); // Fetch another meme if URL has already been fetched
       } else {
@@ -15,10 +13,10 @@ function fetchMeme() {
         const div = document.createElement("div");
         div.innerHTML += `<div class="card shadow mt-2 p-2">
                 <div class="card-body p-1">
-                  <h4 class="card-title ml-2">Posted By <span class="text-success">${meme["memes"]["author"]}</span></h4>
-                  <p class="card-text ml-2"><b>Caption:- </b><span class="text-primary">${meme["memes"]["title"]}</span></p>
+                  <h4 class="card-title ml-2">Posted By <span class="text-success">${meme.author}</span></h4>
+                  <p class="card-text ml-2"><b>Caption:- </b><span class="text-primary">${meme.title}</span></p>
                 </div>
-                <img class="card-img-bottom p-1 shadow" style="border-radius:10px;" src="${meme["memes"]["url"]}" alt="Card image cap" width=320px height=350px onerror="this.onerror=null; this.src='error.jpg';">
+                <img class="card-img-bottom p-1 shadow" style="border-radius:10px;" src="${meme.url}" alt="Card image cap" width=320px height=350px onerror="this.onerror=null; this.src='error.jpg';">
               </div>
            `;
         document.getElementById('meme').appendChild(div);
@@ -36,8 +34,9 @@ function mixedmeme() {
     document.getElementById('meme').innerHTML=""
     fetch("https://meme-api.com/gimme/animememes/1")
       .then(response => response.json())
-      .then(meme => {
-        const url = meme["url"];
+      .then(data => {
+        const meme = data.memes[0];
+        const url = meme.url;
         if (fetchedUrls.includes(url)) {
           fetchMeme(); // Fetch another meme if URL has already been fetched
         } else {
@@ -45,10 +44,10 @@ function mixedmeme() {
           const div = document.createElement("div");
           div.innerHTML += `<div class="card">
                   <div class="card-body p-1">
-                    <h4 class="card-title">Posted By <span class="text-success">${meme["author"]}</span></h4>
-                    <p class="card-text">${meme["title"]}</p>
+                    <h4 class="card-title">Posted By <span class="text-success">${meme.author}</span></h4>
+                    <p class="card-text">${meme.title}</p>
                   </div>
-                  <img class="card-img-bottom p-1" src="${meme["url"]}" alt="Card image cap" width=300px height=350px>
+                  <img class="card-img-bottom p-1" src="${meme.url}" alt="Card image cap" width=300px height=350px>
                 </div>
              `;
           document.getElementById('meme').appendChild(div);
@@ -61,4 +60,3 @@ function mixedmeme() {
   }
   
 }
-
